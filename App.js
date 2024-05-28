ba0f9a029760b755551677abec1f96ec5b7a00e0
Romainvilleneuve78 <128373525+Romainvilleneuve78@users.noreply.github.com>
2024-05-28 15:02:26 +0200

    V1

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './src/screen/Home/Home';
-import Urgence from './src/screen/Urgence/Urgence'
+import SignIn from './src/screen/Urgence/SignIn';
+import SignUp from './src/screen/Urgence/SignUp';
+import Urgence from './src/screen/Urgence/Urgence';
+import Interventions from './src/screen/Urgence/Interventions';
+import Compte from './src/screen/Urgence/Compte';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function UrgenceTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Urgence" component={Urgence} />
+      <Tab.Screen name="Interventions" component={Interventions} />
+      <Tab.Screen name="Compte" component={Compte} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Urgence" component={Urgence} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen 
+          name="UrgenceTabs" 
+          component={UrgenceTabs} 
+          options={{ headerShown: false }} // Hide the header for the tab navigator
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
